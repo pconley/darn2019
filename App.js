2b72af8e884b782f35eddacebda7c299f973f5be
Pat Conley <pconley312@gmail.com>
2019-01-08 06:31:38 -0500

    some formatting

--- App.js
+++ App.js
@@ -16,9 +16,6 @@ import update from 'immutability-helper';
 
 import { Button, Header } from 'react-native-elements'
 
-import Blink from './components/Blink'
-import CounterButton from './components/CounterButton'
-
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -31,41 +28,42 @@ export default class App extends Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
+      stage: 'Bidding',
       isLoading: true,
-      total: 10,
+      total_bid: 0,
+      hand_size: 5,
       counters: [
-        {id:1, value:0, title:'pat'},
-        {id:2, value:0, title:'mj'},
-        {id:3, value:0, title:'claire'},
-        {id:4, value:0, title:'ted'},
-        {id:5, value:0, title:'tim'},
+        {id:1, bid:0, name:'pat'},
+        {id:2, bid:0, name:'mj'},
+        {id:3, bid:0, name:'claire'},
+        {id:4, bid:0, name:'ted'},
+        {id:5, bid:0, name:'tim'},
       ]
     };
   }
 
-  _setValue = (counter, newval) => {
-    const changed = update(counter,{value: {$set: newval}})
+  _setbid = (counter, newval) => {
+    const changed = update(counter,{bid: {$set: newval}})
     const index = this.state.counters.findIndex( c => c.id === counter.id );
     const counters = update(this.state.counters, {[index]: { $set: changed }})
-    const total = counters.reduce((sum,x) => sum+x.value, 0 )
-    this.setState({ total: total, counters: counters });
+    const total_bid = counters.reduce((sum,x) => sum+x.bid, 0 )
+    this.setState({ total_bid: total_bid, counters: counters });
   }
 
   _handleIncrement = (counter) => {
-    const newval = counter.value+1
-    this._setValue(counter,newval)
+    const newval = counter.bid+1
+    this._setbid(counter,newval)
   }
 
   _handleDecrement = (counter) => {
-    const newval = Math.max(0,counter.value-1)
-    this._setValue(counter,newval)
+    const newval = Math.max(0,counter.bid-1)
+    this._setbid(counter,newval)
   }
 
-  _clearValue = (counter) => {
+  _clearbid = (counter) => {
     if (this.state.count === 0) return;
     Alert.alert('You reset the counter!')
-    this._setValue(counter,0)
+    this._setbid(counter,0)
   }
 
   componentDidMount(){
@@ -96,46 +94,56 @@ export default class App extends Component<Props> {
       )
     }
 
-    var titles = 
-        <View style={{paddingTop:80}}>
+    const sample_header =
+      <Header leftComponent={{ icon: 'menu', color: '#fff' }}
+        centerComponent={{ text: 'Oh Darn!', style: { color: '#fff' } }}
+        rightComponent={{ icon: 'home', color: '#fff' }}
+      ></Header>
+
+    const sample_buttons = 
+      <View key='buttons' style={{height: 100, flex: 1, flexDirection: 'row' }}>
+        <Button title='Red Refresh'
+            icon={{name: 'cached'}}
+            buttonStyle={styles.bottomButtonStyle}
+            backgroundColor='red'
+        />
+        <Button
+          title="Loading" loading 
+          buttonStyle={styles.bottomButtonStyle}
+          backgroundColor="rgba(92, 99,216, 1)"
+        />
+      </View>
+
+    var player_rows = 
+        <View style={{paddingTop:20}}>
           <FlatList data={this.state.counters}
               renderItem={({item}) => 
                 <View key={item.id} style={{height: 60, flex: 1, flexDirection: 'row' }}>
-                <CounterButton counter={item}
-                  onIncrement={this._handleIncrement} 
-                  onDecrement={this._handleDecrement} 
-                  onLongPress={this._clearValue}
-                />
-                <Text key={item.id}>{item.title}</Text>
+                  <Button title={String(item.bid)} buttonStyle={styles.bidButtonStyle} />
+                  <Button title='<'
+                          onPress={() => this._handleDecrement(item)} 
+                          onLongPress={() => this._clearbid(item)}
+                          buttonStyle={styles.counterButtonStyle}
+                      />
+                  <Button title='>' 
+                          onPress={() => this._handleIncrement(item)} 
+                          onLongPress={() => this._clearbid(item)}
+                          buttonStyle={styles.counterButtonStyle}
+                      />
+                  <Text>{item.name}</Text>
                 </View>
               }
               keyExtractor={(item, index) => String(item.id)}
           ></FlatList>
         </View>
 
-    console.log(this.state.dataSource)
-
     return(
-      <View style={{ paddingTop:100 }}>
-        <Header
-          leftComponent={{ icon: 'menu', color: '#fff' }}
-          centerComponent={{ text: 'Oh Darn!', style: { color: '#fff' } }}
-          rightComponent={{ icon: 'home', color: '#fff' }}
-        />
-        <View key='buttons' style={{height: 100, flex: 1, flexDirection: 'row' }}>
-            <Button title='Red Refresh'
-                icon={{name: 'cached'}}
-                buttonStyle={styles.buttonStyle}
-                backgroundColor='red'
-            />
-            <Button
-              title="Loading" loading 
-              buttonStyle={styles.buttonStyle}
-              backgroundColor="rgba(92, 99,216, 1)"
-            />
-        </View>
-        <Text>{this.state.total}</Text>
-        {titles}
+      <View style={{ paddingTop:40 }}>
+        {sample_header}
+        <Text style={styles.sectionHeader}>Stage: {this.state.stage}</Text>
+        <Text style={styles.sectionHeader}>Total Bid: {this.state.total_bid} of {this.state.hand_size}</Text>
+        {player_rows}
+        {sample_buttons}
       </View>
 
     );
@@ -163,13 +171,29 @@ const styles = StyleSheet.create({
     fontSize: 18,
     height: 44,
   },
-  buttonStyle: {
-    // backgroundColor: "rgba(92, 99,216, 1)",
+  bidButtonStyle: {
+    // backgroundColor: 'grey',
+    width: 45,
+    height: 45,
+    borderColor: "transparent",
+    borderWidth: 0,
+    borderRadius: 5
+  },
+  bottomButtonStyle: {
     width: 150,
     height: 45,
     borderColor: "transparent",
     borderWidth: 0,
     borderRadius: 5
+  },
+  counterButtonStyle: {
+    backgroundColor: "rgba(92, 99, 216, 1)",
+    margin: 0,
+    width: 40,
+    height: 45,
+    borderColor: "transparent",
+    borderWidth: 0,
+    borderRadius: 15
   }
 })
 
--- components/CounterButton.js
+++ components/CounterButton.js
@@ -7,8 +7,7 @@ export default class CounterButton extends Component {
   render() {
     const { counter } = this.props;
     return (
-      <View style={{height: 60, flex: 1, flexDirection: 'row' }}>
-        <Text>{counter.value}</Text>
+      <View >
         <Button title='<'
             key={100+counter.id} 
             onPress={() => this.props.onDecrement(counter)} 
@@ -27,13 +26,6 @@ export default class CounterButton extends Component {
 }
 
 const styles = StyleSheet.create({
-    button: {
-        margin: 5,
-        width: 50,
-        height: 20,
-        backgroundColor : "grey",
-    },
-    buttonText: { color: "black" },
     buttonStyle: {
       backgroundColor: "rgba(92, 99, 216, 1)",
       margin: 0,
