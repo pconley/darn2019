6ed9153b5025811866f39cdedd69065ae7400a0a
Pat Conley <pconley312@gmail.com>
2019-01-11 07:39:21 -0500

    stable

--- App.js
+++ App.js
@@ -7,6 +7,10 @@ import update from 'immutability-helper';
 
 import { Button, Header } from 'react-native-elements'
 
+export const BIDDING = "Bidding";
+export const PLAYING = "Playing";
+export const SCORING = "Scoring";
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
@@ -16,11 +20,25 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      stage: 'Bidding',
       isLoading: true,
-      total_bid: 0,
-      hand_size: 7,
-      dealer_id: 3,
+      // stage: 'Starting',
+      stage: 'Bidding',
+      current_round_index: 0,
+      tricks: [5, 3, 1, 2, 4],
+      rounds: [
+        { tricks: 5,
+          total_bid: 0,
+          dealer_id: 3,
+          stage: BIDDING,
+          players: [
+            {id:1, bid:0, took:0, name:'pat'},
+            {id:2, bid:0, took:0, name:'mj'},
+            {id:3, bid:0, took:0, name:'claire'},
+            {id:4, bid:0, took:0, name:'ted'},
+            {id:5, bid:0, took:0, name:'tim'},
+          ]
+        }
+      ],
       players: [
         {id:1, bid:0, name:'pat'},
         {id:2, bid:0, name:'mj'},
@@ -32,39 +50,50 @@ export default class App extends Component {
   }
 
   _setbid = (player, value) => {
-    const changed = update(player,{bid: {$set: value}})
-    const index = this.state.players.findIndex( c => c.id === player.id );
-    const players = update(this.state.players, {[index]: { $set: changed }})
-    const total_bid = players.reduce((sum,x) => sum+x.bid, 0 )
-    this.setState({ total_bid: total_bid, players: players });
+    const s_round = this.state.rounds[this.state.current_round_index];
+    const player_index = s_round.players.findIndex( c => c.id === player.id );
+    const x_player = update(player,{bid: {$set: value}});
+    const x_players = update(s_round.players, {[player_index]: { $set: x_player }});
+    const x_total_bid = x_players.reduce((sum,x) => sum+x.bid, 0 );
+    const x_round = update(s_round,{ total_bid: {$set: x_total_bid}, players: {$set: x_players} });
+    const x_rounds = update(this.state.rounds,{[this.state.current_round_index]: {$set: x_round}});
+    this.setState({ rounds: x_rounds });
+  }
+
+  _setStage = (stage) => {
+    this.setState({ stage: stage });
   }
 
   _handleIncrement = (player) => {
-    const newval = player.bid+1
-    this._setbid(player,newval)
+    const newval = player.bid+1;
+    this._setbid(player,newval);
   }
 
   _handleDecrement = (player) => {
-    const newval = Math.max(0,player.bid-1)
-    this._setbid(player,newval)
+    const newval = Math.max(0,player.bid-1);
+    this._setbid(player,newval);
   }
 
   _clearbid = (player) => {
     if (player.bid === 0) return;
     Alert.alert('You reset the bid to zero!')
-    this._setbid(player,0)
+    this._setbid(player,0);
   }
 
   _getDealerMessage = () => { 
-    const bad = this.state.hand_size - this.state.total_bid;
+    const round = this.state.rounds[this.state.current_round_index];
+    const dealer = round.players.find( c => c.id === round.dealer_id );
+    const bad = round.tricks - round.total_bid + dealer.bid;
     if( bad < 0 ) return "can bid anything."
     return "can't bid "+bad;
   }
 
   _getDealerName = () => {
-    const index = this.state.players.findIndex( c => c.id === this.state.dealer_id );
+    const round = this.state.rounds[this.state.current_round_index];
+    const index = round.players.findIndex( c => c.id === round.dealer_id );
     return this.state.players[index].name;
   }
+
 
   componentDidMount(){
     return fetch('https://facebook.github.io/react-native/movies.json')
@@ -94,64 +123,114 @@ export default class App extends Component {
       )
     }
 
+    if( this.state.stage === "Playing" ){
+      return (
+        <View>
+        <Text style={{paddingTop:200}}>Playing</Text>
+        <Button title='Back to Bidding'
+          icon={{name: 'cached'}}
+          onPress={() => this._setStage("Bidding")}
+          buttonStyle={styles.bottomButtonStyle}
+          backgroundColor='red'
+        />
+        </View>
+      )
+    }
+
+    if( this.state.stage === "Starting" ){
+      return(
+        <View style={{paddingTop:200}}>
+          <Button
+            title="Start Game" 
+            onPress={() => this._setStage("Bidding")}
+            buttonStyle={[styles.bottomButtonStyle]}
+            backgroundColor="rgba(92, 99,216, 1)"
+          />     
+        </View>  
+      )
+    }
+
     const sample_header =
       <Header leftComponent={{ icon: 'menu', color: '#fff' }}
         centerComponent={{ text: 'Oh Darn!', style: { color: '#fff' } }}
         rightComponent={{ icon: 'home', color: '#fff' }}
       ></Header>
 
-    const sample_buttons = 
-      <View key='buttons' style={{height: 100, flex: 1, flexDirection: 'row' }}>
-        <Button title='Red Refresh'
-            icon={{name: 'cached'}}
-            buttonStyle={styles.bottomButtonStyle}
-            backgroundColor='red'
-        />
-        <Button
-          title="Loading" loading 
-          buttonStyle={styles.bottomButtonStyle}
-          backgroundColor="rgba(92, 99,216, 1)"
-        />
-      </View>
+    game_buttons = () => { 
+      return (
+       <View key='buttons' style={{paddingTop:30, height: 100, flexDirection: 'row' }}>
+          <Button title='Cancel Game'
+              icon={{name: 'cached'}}
+              onPress={() => this._setStage("Starting")}
+              buttonStyle={styles.bottomButtonStyle}
+              backgroundColor='red'
+          />
+          <Button title="Start Playing" loading 
+              onPress={() => this._setStage("Playing")}
+              buttonStyle={styles.bottomButtonStyle}
+              backgroundColor="rgba(92, 99,216, 1)"
+          />
+        </View>
+      )
+    }
 
-    const player_rows = 
+    player_rows = () => {
+      const dealer = round.players.find( c => c.id === round.dealer_id );
+      return (
         <View style={{paddingTop:20}}>
-          <FlatList data={this.state.players}
-              renderItem={({item}) => 
-
-                <View key={item.id} style={styles.playerRow}>
-                  <Button title='<'
-                          onPress={() => this._handleDecrement(item)} 
-                          onLongPress={() => this._clearbid(item)}
-                          buttonStyle={styles.counterButtonStyle}
-                          containerViewStyle={{ marginLeft: 5, marginRight: 0 }}
-                      />
-                  <Button title={String(item.bid)} 
-                      buttonStyle={styles.bidButtonStyle} 
-                      containerViewStyle={{ marginLeft: 0, marginRight: 0 }}
-                  />
-                  <Button title='>' 
-                          onPress={() => this._handleIncrement(item)} 
-                          onLongPress={() => this._clearbid(item)}
-                          buttonStyle={styles.counterButtonStyle}
-                          containerViewStyle={{ marginLeft: 0, marginRight: 10 }}
-                      />
-                  <Text style={styles.playerName}>{item.name}</Text>
-                </View>
-              }
+          <FlatList data={this.state.rounds[this.state.current_round_index].players}
+              renderItem={player_row}
               keyExtractor={(item, index) => String(item.id)}
           ></FlatList>
-        </View>
+      </View> );
+    }
 
+    player_row = ({item}) => {
+      const round = this.state.rounds[this.state.current_round_index];
+      const dealer = round.players.find( c => c.id === round.dealer_id );
+      const textColor = item.id === dealer.id ? "red" : "black";
+      return (
+          <View key={item.id} style={styles.playerRow}>
+            <Button title='<'
+                    onPress={() => this._handleDecrement(item)} 
+                    onLongPress={() => this._clearbid(item)}
+                    buttonStyle={styles.counterButtonStyle}
+                    containerViewStyle={{ marginLeft: 5, marginRight: 0 }}
+                />
+            <Button title={String(item.bid)} 
+                buttonStyle={styles.bidButtonStyle} 
+                containerViewStyle={{ marginLeft: 0, marginRight: 0 }}
+            />
+            <Button title='>' 
+                    onPress={() => this._handleIncrement(item)} 
+                    onLongPress={() => this._clearbid(item)}
+                    buttonStyle={styles.counterButtonStyle}
+                    containerViewStyle={{ marginLeft: 0, marginRight: 10 }}
+                />
+            <Text style={[styles.playerName, {color: textColor}]}>{item.name}</Text>
+          </View>
+      )
+    }
+
+    info_bar = () => {
+      return (
+        <View>
+          <Text style={styles.topline}>Tricks: {round.tricks}</Text>
+          <Text style={styles.topline}>Players: {round.players.length}</Text>
+          <Text style={styles.topline}>Stage: {round.stage}</Text>
+          <Text style={styles.topline}>Total Bid: {round.total_bid} of {round.tricks}</Text>
+          <Text style={[styles.topline, styles.red]}>Dealer [{this._getDealerName()}] {this._getDealerMessage()}</Text>
+        </View>
+      )
+    }
+
+    const round = this.state.rounds[this.state.current_round_index];
     return(
       <View style={{ paddingTop:40 }}>
         {sample_header}
-        <Text style={styles.topline}>Players: {this.state.players.length}</Text>
-        <Text style={styles.topline}>Stage: {this.state.stage}</Text>
-        <Text style={styles.topline}>Total Bid: {this.state.total_bid} of {this.state.hand_size}</Text>
-        <Text style={[styles.topline, styles.red]}>Dealer [{this._getDealerName()}] {this._getDealerMessage()}</Text>
-        {player_rows}
-        {sample_buttons}
+        {info_bar()}
+        {player_rows()}
+        {game_buttons()}
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
